f7a179fad92d977f3ca8944f5170563b73ba0f6d
Eknox85 <eknox85@lakers.mercyhurst.edu>
2025-04-21 18:26:13 -0400

    Ran collectstatic

--- myproject/settings.py
+++ myproject/settings.py
@@ -133,10 +133,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
 STATIC_URL = '/static/'
-staticfiles_dirs = [os.path.join(BASE_DIR, 'templates')]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
